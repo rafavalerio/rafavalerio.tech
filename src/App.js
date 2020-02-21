5bcfdefe0c9ca56f718d8340a99fbd43f561ee52
Rafael Valerio <rafvalerio26@gmail.com>
2020-02-22 10:41:39 +1100

    Add React

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+function App() {
+  return <AppWrapper>Hi.</AppWrapper>;
+}
+
+export default App;
